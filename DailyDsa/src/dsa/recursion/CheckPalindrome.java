e9c500653d6a04d0f4e2e8b2d22ee75c3023b97f
swapnajitpanda94 <swapnajitpanda94@gmail.com>
2025-12-27 20:09:01 +0530

    palindrome problem

---
+++ DailyDsa/src/dsa/recursion/CheckPalindrome.java
@@ -0,0 +1,30 @@
+package dsa.recursion;
+
+public class CheckPalindrome {
+    public static void main(String[] args) {
+        String str = "MADAM";
+        System.out.println("Is palindrome ? "+isPalindromeIterativeWay(str) );
+        System.out.println("Is palindrome ? "+isPalindromeRecursiveWay(str,0, str.length()-1) );
+
+    }
+
+    private static boolean isPalindromeRecursiveWay(String str, int i, int j) {
+        if(i >j)
+            return true;
+        return ( str.charAt(i) == str.charAt(j) ) && isPalindromeRecursiveWay(str, i+1 , j-1);
+    }
+
+
+    private static boolean isPalindromeIterativeWay(String str) {
+        int i =0 ;
+        int j= str.length()-1;
+        while(i <= j){
+           if(str.charAt(i) != str.charAt(j)){
+               return false;
+           }
+           i++;
+           j--;
+        }
+        return true ;
+    }
+}
