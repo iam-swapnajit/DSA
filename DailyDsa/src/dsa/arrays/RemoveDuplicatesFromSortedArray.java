457a0e2cd5e75f2fc5693e494555f82df06ef21d
swapnajitpanda94 <swapnajitpanda94@gmail.com>
2026-01-03 10:35:57 +0530

    remove duplicate element in a sorted array leet code problem

---
+++ DailyDsa/src/dsa/arrays/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,31 @@
+package dsa.arrays;
+
+import java.util.Arrays;
+
+/**
+ * <a href="https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/">...</a>
+ */
+public class RemoveDuplicatesFromSortedArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,2,3};
+        int x = removeDuplicates(arr);
+        System.out.println(x);
+    }
+
+
+    private static int removeDuplicates(int[] arr) {
+        int uniqueIndex =0;
+        /*if(arr.length == 1){
+            return 1;
+        }*/
+        for(int index = 0; index <arr.length; index++){
+            if(arr[index] > arr[uniqueIndex]){
+                uniqueIndex = uniqueIndex+1;
+                arr[uniqueIndex] = arr[index];
+            }
+
+        }
+        System.out.println(Arrays.toString(arr));
+        return  uniqueIndex+1;
+    }
+}
