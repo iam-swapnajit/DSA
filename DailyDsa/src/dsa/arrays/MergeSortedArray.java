d37f31383e821459c7b4b0f54c5e564b3620ea2d
swapnajitpanda94 <swapnajitpanda94@gmail.com>
2026-01-10 13:17:54 +0530

    mergeWithoutUsingNewArray

--- DailyDsa/src/dsa/arrays/MergeSortedArray.java
+++ DailyDsa/src/dsa/arrays/MergeSortedArray.java
@@ -11,7 +11,7 @@ public class MergeSortedArray {
         int m = 3;
         int[] nums2 = {2,5,6};
         int n=3;
-        merge(nums1,m,nums2,n);
+        mergeWithoutUsingNewArray(nums1,m,nums2,n);
         System.out.println(Arrays.toString(nums1));
     }
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
@@ -30,6 +30,26 @@ public class MergeSortedArray {
                 ptr2++;
             }
         }
+
+    }
+
+    public static void mergeWithoutUsingNewArray(int[] nums1, int m, int[] nums2, int n) {
+        int ptr1 = m-1;
+        int ptr2 =n-1;
+
+        for(int i=m+n-1; i>=0 ; i--){
+            if(ptr2<0) break;// Means nums2 already been traversed and the remaining in nums1 is already sorted
+            if(ptr1>=0 && nums1[ptr1]>nums2[ptr2]){
+                nums1[i] =  nums1[ptr1];
+                ptr1--;
+
+            }
+            else {
+                nums1[i] = nums2[ptr2];
+                ptr2--;
+            }
+        }
+        System.out.println(Arrays.toString(nums1));
 
     }
 }
