df7620e8f0f1c1c616af8c5c215fb93a8c7b41cd
swapnajitpanda94 <swapnajitpanda94@gmail.com>
2026-01-17 11:18:37 +0530

    two pointer problem

---
+++ DailyDsa/src/dsa/twopointers/TwoSum.java
@@ -0,0 +1,28 @@
+package dsa.twopointers;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {4,3,7,2};
+        int target = 6;
+        int[] elements = twoSum(nums, target);
+        System.out.println(Arrays.toString(elements));
+
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> meta = new HashMap<>();
+
+        for(int i =0 ; i<nums.length ; i++){
+            if(meta.containsKey(target-nums[i])){
+                return new int[]{i, meta.get(target-nums[i])};
+            }else{
+                meta.put(nums[i],i);
+            }
+        }
+
+        return new int[]{-1,-1};
+    }
+}
