f7d998de16e4fca7e361d972604832e3d398104c
swapnajitpanda94 <swapnajitpanda94@gmail.com>
2026-01-18 18:14:34 +0530

    Find first occurance leetdoce problem

---
+++ DailyDsa/src/dsa/slidingwindow/FindTheFirstOccurence.java
@@ -0,0 +1,20 @@
+package dsa.slidingwindow;
+
+public class FindTheFirstOccurence {
+    public static void main(String[] args) {
+        String haystack = "ssand", needle = "ad";
+        System.out.println(strStr(haystack, needle));
+    }
+    public static int strStr(String haystack, String needle) {
+        int i = 0;
+        int j = needle.length();
+
+        while(i+j < haystack.length()+1){
+            if(haystack.substring(i, i+j).equals(needle)){
+                return i ;
+            }
+            i++;
+        }
+        return -1 ;
+    }
+}
