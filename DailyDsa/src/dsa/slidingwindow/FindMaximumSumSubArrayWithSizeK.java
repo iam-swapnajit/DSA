356928aae092369c8fe2660d5100f49e21592be6
swapnajitpanda94 <swapnajitpanda94@gmail.com>
2026-02-14 22:33:51 +0530

    maximum sum subarray sliding window problem

---
+++ DailyDsa/src/dsa/slidingwindow/FindMaximumSumSubArrayWithSizeK.java
@@ -0,0 +1,50 @@
+package dsa.slidingwindow;
+
+public class FindMaximumSumSubArrayWithSizeK {
+    public static void main(String[] args) {
+        int[] arr = {200,300,400,500};
+        System.out.println(findMaximumSum2(arr, 2));
+    }
+
+    public static int findMaximumSum(int[] arr , int k){
+        if (arr == null || k <= 0 || arr.length < k)
+            throw new IllegalArgumentException("Invalid input");
+        int maxSum = 0;
+        int sum = 0 ;
+
+        for(int i=0 ; i<k ; i++){
+            sum += arr[i];
+        }
+        maxSum = sum;
+        //sliding window
+        for(int i=k ; i< arr.length ; i++){
+            sum = sum - arr[i-k] + arr[i];
+            maxSum = Math.max(maxSum, sum);
+        }
+        return maxSum;
+    }
+
+    public static int findMaximumSum2(int[] arr , int k){
+        if (arr == null || k <= 0 || arr.length < k)
+            throw new IllegalArgumentException("Invalid input");
+        int maxSum = 0;
+        int sum = 0 ;
+
+        for(int i=0 ; i<k ; i++){
+            sum += arr[i];
+        }
+
+        int low =0;
+        int high =k-1;
+        //sliding window
+        while(true){
+            maxSum = Math.max(maxSum, sum);
+            low++;
+            high++;
+            if(high == arr.length) break;
+            sum = sum + arr[high] - arr[low-1];
+        }
+
+        return maxSum;
+    }
+}
